bbc27343bc2867f8732ce2d747aec6240427a7ae
Anuraja <44002167+Anulali@users.noreply.github.com>
2019-05-27 21:17:02 +0530

    Update greatest.py

--- greatest.py
+++ greatest.py
@@ -1,6 +1,6 @@
-num1=int(input("enter the num1: "))
-num2=int(input("enter the num2: "))
-num3=int(input("enter the num3: "))
+num1=input(" ")
+num2=input(" ")
+num3=input(" ")
 if(num1>num2 and num1>num3):
   print(num1)
 elif(num2>num1 and num2>num3):
