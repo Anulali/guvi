b0a75c7c5df3674db78092c7ebbaf46082ab0ca9
Anuraja <44002167+Anulali@users.noreply.github.com>
2019-05-27 22:54:00 +0530

    Update greatest.py

--- greatest.py
+++ greatest.py
@@ -1,9 +1,12 @@
 num1=input(" ")
 num2=input(" ")
 num3=input(" ")
-if(num1>num2 and num1>num3):
-  print(num1)
-elif(num2>num1 and num2>num3):
+if(num1>num2):
+  if(num1>num3):
+    print(num1)
+  else:
+    print(num3)  
+elif(num2>num3):
   print(num2)
 else:
   print(num3)
