14d27f6c8059c8a07b246b83334c9dff080824e6
Anuraja <44002167+Anulali@users.noreply.github.com>
2019-05-29 13:15:56 +0530

    Create arm.py

---
+++ arm.py
@@ -0,0 +1,15 @@
+n1,n2=(input().split())
+a=int(n1)
+b=int(n2)
+temp=0
+for i in range(a,b+1):
+    sum==0
+    temp==i
+    while(temp>0):
+      x=temp%10
+      sum=sum+x**3
+      temp=temp//10
+    if(sum==i):
+      print(i)
+
+  
